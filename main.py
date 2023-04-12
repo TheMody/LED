c9e3761eafa9b76244ac81fddea88a95a16b0900
PhilipKenneweg <p.kenneweg@unitybox.de>
2023-04-12 19:38:15 +0200

    zeug

--- main.py
+++ main.py
@@ -148,14 +148,14 @@ if __name__ == '__main__':
         new_data = 0
 
         def callback(indata, frames, time, status):
-            global soundarray, new_data#, running_avg_long, running_avg_short, running_avg_medium
+            global soundarray, new_data, running_avg_long, running_avg_short, running_avg_medium
             soundarray = np.append(soundarray, indata[np.arange(0, len(indata), 2)])
             new_data = new_data + int(len(indata)/2)
-            # for i in indata:
-            #     i = abs(i)
-            #     running_avg_long = running_avg_long * beta1 + i *(1-beta1)
-            #     running_avg_medium = running_avg_medium * beta2 + i *(1-beta2)
-            #     running_avg_short = running_avg_short * beta3 + i *(1-beta3)
+            for i in indata:
+                i = abs(i)
+                running_avg_long = running_avg_long * beta1 + i *(1-beta1)
+                running_avg_medium = running_avg_medium * beta2 + i *(1-beta2)
+                running_avg_short = running_avg_short * beta3 + i *(1-beta3)
 
         print("samplerate: ", fs)
         with sd.InputStream(channels=1, callback=callback,  samplerate=fs):
@@ -258,16 +258,16 @@ if __name__ == '__main__':
                                 strip.setPixelColor(i,  Color(bright, bright, bright))
                             strip.show()
                         start = time.time()
-                        onsets, size = AmplitudeBasedOnsets(soundarray, distance=10, prominence=0.3, window_size=512)
-                        print(time.time()-start)
-                        print(len(soundarray))
-                        if len(onsets) > 0 :
-                            if onsets[-1] >  (len(soundarray) - new_data) and ((time.time()-waittime) > 2000/fs):
-                               # print("onset", onsets, size)
-                             #   print("TIMEsince lasts",(time.time()-waittime) )
-                                waittime = time.time()
-                              #  print("onset", onsets, size)
-                                j = j+1
+                      #  onsets, size = AmplitudeBasedOnsets(soundarray, distance=10, prominence=0.3, window_size=512)
+                       # print(time.time()-start)
+                      #  print(len(soundarray))
+                        # if len(onsets) > 0 :
+                        #     if onsets[-1] >  (len(soundarray) - new_data) and ((time.time()-waittime) > 2000/fs):
+                        #        # print("onset", onsets, size)
+                        #      #   print("TIMEsince lasts",(time.time()-waittime) )
+                        #         waittime = time.time()
+                        #       #  print("onset", onsets, size)
+                        #         j = j+1
                         if detect_sudden_change(soundarray) & ((time.time()-waittime2) > 0.500):
                             mode = mode +1
                             waittime2 = time.time()
